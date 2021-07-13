cd3129a6d050a5f44295f4e137a8f18a62f30195
tina250594 <tina250594@gmail.com>
2021-07-13 14:28:01 +0300

    Added markup in side-filter-form

--- src/app/core/components/side-filter-form/side-filter-form.component.ts
+++ src/app/core/components/side-filter-form/side-filter-form.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+import {Component, OnInit, ChangeDetectionStrategy, ViewChild, ElementRef} from '@angular/core';
+import {FormControl} from "@angular/forms";
+import {Observable} from "rxjs";
+import {map, startWith} from "rxjs/operators";
+import {COMMA, ENTER} from "@angular/cdk/keycodes";
+import {MatChipInputEvent} from "@angular/material/chips";
+import {MatAutocompleteSelectedEvent} from "@angular/material/autocomplete";
 
 @Component({
   selector: 'app-side-filter-form',
@@ -7,10 +13,88 @@ import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class SideFilterFormComponent implements OnInit {
+  public streets: string[] = ['Champs-Élysées', 'Lombard Street', 'Abbey Road', 'Fifth Avenue'];
+  public filteredStreets: Observable<string[]>;
+  public removable = true;
+  public separatorKeysCodes: number[] = [ENTER, COMMA];
+  public filteredCategories: Observable<string[]>;
+  public categories: string[] = ['Teacher'];
+  public allCategories: string[] = ['Teacher', 'Doctor', 'Dentist', 'Trainer', 'Footballer'];
 
-  constructor() { }
+  public fruitCtrl = new FormControl();
+  public control = new FormControl();
 
-  ngOnInit(): void {
+  @ViewChild('fruitInput') fruitInput: ElementRef<HTMLInputElement>;
+
+  ngOnInit() {
+    this.filteredStreets = this.control.valueChanges.pipe(
+      startWith(''),
+      map(value => this._filterStreets(value))
+    );
+    this.filteredCategories = this.fruitCtrl.valueChanges.pipe(
+      startWith(null),
+      map((category: string | null) => category ? this._filterCategories(category) : this.allCategories.slice()));
   }
 
+  // private _filter(value: string): string[] {
+  //   const filterValue = this._normalizeValue(value);
+  //   return this.streets.filter(street => this._normalizeValue(street).includes(filterValue));
+  // }
+
+  // private _normalizeValue(value: string): string {
+  //   return value.toLowerCase().replace(/\s/g, '');
+  // }
+
+  add(event: MatChipInputEvent): void {
+    const value = (event.value || '').trim();
+
+    if (value) {
+      this.categories.push(value);
+    }
+
+    event.chipInput.clear();
+
+    this.fruitCtrl.setValue(null);
+  }
+
+  remove(fruit: string): void {
+    const index = this.categories.indexOf(fruit);
+
+    if (index >= 0) {
+      this.categories.splice(index, 1);
+    }
+  }
+
+  selected(event: MatAutocompleteSelectedEvent): void {
+    this.categories.push(event.option.viewValue);
+    this.fruitInput.nativeElement.value = '';
+    this.fruitCtrl.setValue(null);
+  }
+
+  private _filterStreets(value: string): string[] {
+    const filterValue = value.toLowerCase();
+    return this.streets.filter(street => street.toLowerCase().includes(filterValue));
+  }
+
+  private _filterCategories(value: string): string[] {
+    const filterValue = value.toLowerCase();
+
+    return this.allCategories.filter(category => category.toLowerCase().includes(filterValue));
+  }
+
+  onSubmit() {
+  //  submit form
+  }
+
+  onClear() {
+  //  clear field in form
+  }
+
+  onCancel() {
+  //  cancel fill form
+  }
+
+  onReturn() {
+    // return to top component
+  }
 }
--- src/app/core/components/side-filter-form/side-filter-form.module.ts
+++ src/app/core/components/side-filter-form/side-filter-form.module.ts
@@ -1,20 +1,48 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { SideFilterFormComponent } from './side-filter-form.component';
-import {SideFilterHeaderModule} from "@app/core/components/side-filter-header/side-filter-header.module";
-
-
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {SideFilterFormComponent} from './side-filter-form.component';
+import {SideFilterHeaderModule} from '@app/core/components/side-filter-header/side-filter-header.module';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {ReactiveFormsModule} from '@angular/forms';
+import {MatInputModule} from '@angular/material/input';
+import {MatAutocompleteModule} from "@angular/material/autocomplete";
+import {MatChipsModule} from "@angular/material/chips";
+import {MatIconModule} from "@angular/material/icon";
+import {MatCheckboxModule} from "@angular/material/checkbox";
+import {CheckboxModule} from "@app/shared/components/checkbox/checkbox.module";
+import {MatSlideToggleModule} from "@angular/material/slide-toggle";
+import {MatRadioModule} from "@angular/material/radio";
+import {RadioButtonModule} from "@app/shared/components/radio-button/radio-button.module";
+import {MatExpansionModule} from "@angular/material/expansion";
+import {SvgIconModule} from "@app/shared/components/svg-icon/svg-icon.module";
+import {SideFilterFooterModule} from "@app/core/components/side-filter-footer/side-filter-footer.module";
 
 @NgModule({
   declarations: [
     SideFilterFormComponent
   ],
-    imports: [
-        CommonModule,
-        SideFilterHeaderModule
-    ],
+  imports: [
+    CommonModule,
+    SideFilterHeaderModule,
+    SideFilterFooterModule,
+    MatFormFieldModule,
+    MatIconModule,
+    MatInputModule,
+    MatCheckboxModule,
+    CheckboxModule,
+    MatSlideToggleModule,
+    MatRadioModule,
+    RadioButtonModule,
+    MatChipsModule,
+    ReactiveFormsModule,
+    MatAutocompleteModule,
+    MatExpansionModule,
+    SvgIconModule,
+  ],
   exports: [
     SideFilterFormComponent
   ]
 })
-export class SideFilterFormModule { }
+
+export class SideFilterFormModule {
+}
